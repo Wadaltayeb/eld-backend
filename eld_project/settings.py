572230b56d668f67885d1c5d94ba4d887743d0cd
Ali <li096732@gmail.com>
2025-09-19 13:04:59 +0200

    ✅ Fixed template path for home.html rendering

--- eld_project/settings.py
+++ eld_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'eld_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        ''DIRS': [BASE_DIR / "eld_project" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
